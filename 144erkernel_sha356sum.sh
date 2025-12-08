c480712151012a2bf2ac8f198ea835fc9bd0937f
CB <christoph.bellmann@gmail.com>
2025-12-08 21:54:32 +0100

    cleanup

--- 144erkernel_sha356sum.sh
+++
@@ -1,2 +0,0 @@
-sudo sha256sum /boot/initrd.img-5.15.0-144-generic  
-sudo sha256sum /media/christoph/some_space/timeshift/snapshots/2025-11-21_01-00-01/localhost/boot/initrd.img-5.15.0-144-generic 
